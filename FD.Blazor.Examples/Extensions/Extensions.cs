d6c2ee85e83f785621e0d864182c6f52dac9d44a
Francisco Araujo <franciscoa@mcrinc.com>
2021-06-16 15:56:23 -0400

    Added a extension to format a column to do additional tests get getting property name and sorting

---
+++ FD.Blazor.Examples/Extensions/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FD.Blazor.Examples
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Converts the seconds to an min seconds display string.
+        /// </summary>
+        /// <param name="timeSpan">The time span.</param>
+        /// <returns>
+        /// A string in the format minutes'seconds''.
+        /// </returns>
+        public static string DurationToString(this TimeSpan timeSpan)
+        {
+            var s = TimeSpan.FromSeconds(timeSpan.TotalSeconds);
+
+            return string.Format("{0}'{1}''", (int)s.TotalMinutes, s.Seconds);
+        }
+    }
+}
