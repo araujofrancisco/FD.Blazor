da783e3898747793c48d78e84e4caee07038d06d
Francisco Araujo <franciscoa@mcrinc.com>
2021-07-16 22:38:06 -0400

    Add Task Queue based on a ProducerConsumerQueue

---
+++ FD.Blazor.Core/TaskQueue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FD.Blazor.Core
+{
+    public class TaskQueue
+    {
+        private readonly ProducerConsumerQueue _pcQ;
+        private readonly Dictionary<Guid, Task> _qTasks;
+
+        public event EventHandler<Guid> QueueTask;
+
+        public TaskQueue(int? workers = 1)
+        {
+            _pcQ = new((int)workers);
+            _qTasks = new();
+
+            _pcQ.StartingTask += OnStartingTask;
+            _pcQ.CompletedTask += OnCompletedTask;
+        }
+
+        protected virtual void OnStartingTask(object sender, Guid e)
+        {
+#if DEBUG
+            Console.WriteLine($"Starting task: {e}");
+#endif
+        }
+
+        protected virtual void OnCompletedTask(object sender, Guid e)
+        {
+#if DEBUG
+            Console.WriteLine($"Completed task: {e}");
+#endif
+        }
+
+        public Task AddTask(Action action)
+        {
+            Guid guid = Guid.NewGuid();
+
+            Task newTask = _pcQ.EnqueueTask(guid, action);
+            _qTasks.Add(guid, newTask);
+            QueueTask?.Invoke(newTask, guid);
+
+            return newTask;
+        }
+
+        public int Count() =>
+            _qTasks.Count;
+
+        /// <summary>
+        /// Find a task by guid, returning true if found and setting output to task parameter.
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <param name="task"></param>
+        /// <returns></returns>
+        public bool GetTask(Guid guid, out Task task) =>
+            _qTasks.TryGetValue(guid, out task);
+
+        public bool RemoveTask(Guid guid, out Task task) =>
+            _qTasks.Remove<Guid, Task>(guid, out task);
+    }
+}
