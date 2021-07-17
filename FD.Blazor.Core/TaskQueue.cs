8515bf26b9af0e8cffef91d19e9361a6a2b8ea7c
Francisco Araujo <franciscoa@mcrinc.com>
2021-07-17 17:00:32 -0400

    Improve CompletedTask event handler to remove task from tracking dictionary

--- FD.Blazor.Core/TaskQueue.cs
+++ FD.Blazor.Core/TaskQueue.cs
@@ -4,6 +4,11 @@ using System.Threading.Tasks;
 
 namespace FD.Blazor.Core
 {
+    /// <summary>
+    /// Task Queue implemented using a ProducerConsumerQueue cascading Starting/Completed task events. Adds a Guid id for tasks queue allowing to 
+    /// get the task with it.
+    /// </summary>
+    /// <typeparam name="T">Type of return for tasks queue</typeparam>
     public class TaskQueue<T>
         where T : new()
     {
@@ -28,10 +33,19 @@ namespace FD.Blazor.Core
         protected virtual void OnStartingTask(object sender, Guid e) =>
             StartingTask?.Invoke(sender, e);
 
-        protected virtual void OnCompletedTask(object sender, Guid e) =>
+        protected virtual void OnCompletedTask(object sender, Guid e)
+        {
+            // removes task from tracking dictionary and trigger completion event
+            RemoveTask(e, out Task task);
             CompletedTask?.Invoke(sender, e);
+        }
+        public Task Enqueue(Func<T> function) =>
+            EnqueueTask(function);
 
-        public Task Enqueue(Func<Task<T>> function)
+        public Task Enqueue(Func<Task<T>> function) =>
+            EnqueueTask(function);
+        
+        private Task EnqueueTask(dynamic function)
         {
             Guid guid = Guid.NewGuid();
 
@@ -42,6 +56,9 @@ namespace FD.Blazor.Core
             return newTask;
         }
 
+        public void CompleteAdding()
+            => _pcQ.CompleteAdding();
+
         /// <summary>
         /// Find a task by guid, returning true if found and setting output to task parameter.
         /// </summary>
@@ -51,6 +68,12 @@ namespace FD.Blazor.Core
         public bool GetTask(Guid guid, out Task task) =>
             _qTasks.TryGetValue(guid, out task);
 
+        /// <summary>
+        /// Remove a task from tracking dictionary leaving task in the queue.
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <param name="task"></param>
+        /// <returns></returns>
         public bool RemoveTask(Guid guid, out Task task) =>
             _qTasks.Remove<Guid, Task>(guid, out task);
     }
